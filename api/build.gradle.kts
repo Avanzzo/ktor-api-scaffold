f7494b550569b92c0ac9d8c01e75effbbbf3424e
FernandoAvanzo <fernando.avanzo@gmail.com>
2024-10-12 13:15:07 -0300

    Update to ktor3

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -2,10 +2,13 @@
 
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val kotlinVersion: String by project
+val logbackVersion: String by project
+
 plugins {
     kotlin("jvm") version "2.0.20"
     application
-    id("io.ktor.plugin") version "2.3.12"
+    id("io.ktor.plugin") version "3.0.0"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
 }
 
@@ -26,17 +29,19 @@ repositories {
 dependencies {
     implementation("com.google.guava:guava:32.0.0-android")
     implementation("org.slf4j:slf4j-simple:2.0.9")
-    implementation("io.ktor:ktor-server-core:2.3.12")
-    implementation("io.ktor:ktor-server-netty:2.3.12")
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("io.ktor:ktor-server-config-yaml")
     implementation("io.ktor:ktor-server-content-negotiation:2.3.12")
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.12")
     implementation("io.insert-koin:koin-ktor:3.5.6")
     implementation("io.insert-koin:koin-core:3.5.6")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
 
     testImplementation(kotlin("test"))
     testImplementation("io.ktor:ktor-server-tests:2.3.12")
     testImplementation("io.ktor:ktor-server-test-host:2.3.12")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:2.0.20")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:$kotlinVersion")
     testImplementation("io.mockk:mockk:1.13.4")
     testImplementation("io.ktor:ktor-client-content-negotiation:2.3.12")
     testImplementation("io.insert-koin:koin-test-junit5:3.5.6")
